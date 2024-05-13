023fe0090ad251cb23a6c575079b0ecba653610c
Catarino Carmona <carmonc1@gmatc.matc.edu>
2024-05-13 15:57:14 -0500

    created List/List.js, ListItem.js, index.js files

--- blog/src/BaseContainer/index.js
+++ blog/src/BaseContainer/index.js
@@ -1,14 +1,1 @@
-export * from './BaseContainer'import React from 'react'
-import PropTypes from 'prop-types'
-import { Flex, Box } from 'rebass'
-
-export const BaseContainer =({children, flex =false, ...rest})=> {
-    let Component = flex ? Flex : Box
-    return <Component {...rest}>{children}</Component>
-}
-
-BaseContainer.propTypes= {
-    flex: PropTypes.bool,
-    as: PropTypes.string,
-    children: PropTypes.node.isRequired
-}
+export * from './BaseContainer'
