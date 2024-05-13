cb36154bd38d6e3b5ae9a10f318fe5403c8832e1
Catarino Carmona <carmonc1@gmatc.matc.edu>
2024-05-13 07:23:13 -0500

    updated

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Best bikes`,
+    description: ` `,
+    author: `@Catarino Carmona L.`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact:{
+      name: `Catarino Carmona L.`,
+      company: `Products review`, 
+      address: `123 Main street, Milwaukee, WI, 15300`
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
