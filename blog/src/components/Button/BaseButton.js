b00b92a9f2b1a25fc5772189284bb355892ae679
Catarino Carmona <carmonc1@gmatc.matc.edu>
2024-05-17 16:40:59 -0500

    completed

--- blog/src/components/Button/BaseButton.js
+++ blog/src/components/Button/BaseButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Button} from 'rabass'
+import {Button} from 'rebass'
 import styled from 'styled-components'
 
 const Reset = styled(Button)`
